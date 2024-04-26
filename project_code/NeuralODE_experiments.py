f5a5648e7adc08cdeed88059353818ddcb3e84bd
root <root@apiasecz.1.1.1.1>
2024-04-26 01:46:14 +0000

    finish all

--- project_code/NeuralODE_experiments.py
+++ project_code/NeuralODE_experiments.py
@@ -66,84 +66,71 @@ def experiment1(t_eval):
     return
 
 # Training NeuralODE to fit a spiral 
-def experiment2(n_epoch, file_name):
+def experiment2(n_epoch, batch_size, plot_freq, file_name):
     # First set the ode solver to the predetermined ODESolver object
     ode_solver = ODESolver()                                # ODE Solver
+    # Spiral parameters
     spiral_matrix = torch.Tensor([[-0.1, 2.], [-2., -0.1]]) # Matrix that describes the generated spiral
-    z_init = Variable(torch.Tensor([[-4.0, -2.0]]))         # Starting point for the experiment for generating spiral
-    
-    def conduct_experiment(ode_true, ode_trained, n_steps, name, plot_freq = 10, ode_solver = ode_solver):
-        # Create data
-        z0 = Variable(torch.Tensor([[-4.0, -2.0]]))
+    z_init = Variable(torch.Tensor([[-4.0, -2.0]]))         # Initial starting point for the spiral
 
-        t_max = 8 * np.pi
-        n_points = 200
-
-        index_np = np.arange(0, n_points, 1, dtype=np.int64)
-        index_np = np.hstack([index_np[:, None]])
-        times_np = np.linspace(0, t_max, num=n_points)
-        times_np = np.hstack([times_np[:, None]])
-
-        times = torch.from_numpy(times_np[:, :, None]).to(z0)
-        obs = ode_true(z0, times, save_all=True, ode_solver = ode_solver).detach()
-        obs = obs + torch.randn_like(obs) * 0.01
-
-        # Get trajectory of random timespan 
-        min_delta_time = 1.0
-        max_delta_time = 5.0
-        max_points_num = 32
-        def create_batch():
-            t0 = np.random.uniform(0, t_max - max_delta_time)
-            t1 = t0 + np.random.uniform(min_delta_time, max_delta_time)
-
-            idx = sorted(np.random.permutation(index_np[(times_np > t0) & (times_np < t1)])[:max_points_num])
-
-            obs_ = obs[idx]
-            ts_ = times[idx]
-            return obs_, ts_
-
-        # Train Neural ODE
-        optimizer = torch.optim.Adam(ode_trained.parameters(), lr = 0.01)
-        for i in range(n_steps):
-            obs_, ts_ = create_batch()
-
-            z_ = ode_trained(obs_[0], ts_, save_all=True, ode_solver = ode_solver)
-            loss = F.mse_loss(z_, obs_.detach())
-
-            optimizer.zero_grad()
-            loss.backward(retain_graph=True)
-            optimizer.step()
-
-            if i % plot_freq == 0:
-                z_p = ode_trained(z0, times, save_all = True, ode_solver = ode_solver)
-                plot_ODE_sol(observations = [obs], times = [times], pred_path = [z_p], figname = f'./pictures/{name}_{i}.png')
-                print(f'Epoch: {i}, loss: {loss}')
-    
-    
-    # Restrict ODE to a linear function
+    # Linear ODE function to structure (will be used in our neural net) 
     class LinearODEF(ODEFunction):
         def __init__(self, W):
             super(LinearODEF, self).__init__()
-            self.lin = nn.Linear(2, 2, bias=False)
+            self.lin = nn.Linear(2, 2, bias = False)
             self.lin.weight = nn.Parameter(W)
 
         def forward(self, x, t):
             return self.lin(x)
 
-    # True function
+    # True function for generating the data (spiral)
     class SpiralFunction(LinearODEF):
         def __init__(self):
             super(SpiralFunction, self).__init__(spiral_matrix)
             
-    # Random initial guess for function
+    # Random initial guess for function (random weights, correct structure)
     class TrainLinearODEF(LinearODEF):
         def __init__(self):
             super(TrainLinearODEF, self).__init__(torch.randn(2, 2)/2.)
 
+    # Define the Neural ODEs with the predefined function structure
+    # One for creating true data
+    # One for training to find correct parameters
     ode_true = NeuralODE(SpiralFunction())
     ode_trained = NeuralODE(TrainLinearODEF())
+    
+    # Create data
+    t_max = 8 * np.pi                               # Max time for generating the path
+    n_points = 200                                  # Number of points generated in the path
 
-    conduct_experiment(ode_true, ode_trained, n_epoch, file_name)
+    # Index and times numpy data
+    index_np = np.arange(0, n_points, 1, dtype = np.int64)
+    index_np = np.hstack([index_np[:, None]])
+    times_np = np.linspace(0, t_max, num = n_points)
+    times_np = np.hstack([times_np[:, None]])
+    
+    # Then tensor times data, and generated observation data
+    times = torch.from_numpy(times_np[:, :, None]).to(z_init)
+    observations = ode_true(z_init, times, save_all = True, ode_solver = ode_solver).detach()
+    observations = observations + torch.randn_like(observations) * 0.01 # Add some randomness to the data
+
+    # Train Neural ODE
+    optimizer = torch.optim.Adam(ode_trained.parameters(), lr = 0.01)
+    for i in range(n_epoch):
+        # Get the batched data
+        observation_batch, time_batch = create_batch(observations, times_np, times, t_max, index_np, batch_size)
+        # Predict and update model
+        pred_data = ode_trained(observation_batch[0], time_batch, save_all = True, ode_solver = ode_solver)
+        loss = F.mse_loss(pred_data, observation_batch.detach())
+        optimizer.zero_grad()
+        loss.backward(retain_graph = True) # Uses Adjoint method
+        optimizer.step()
+
+        # Plot ever so often
+        if i % plot_freq == 0:
+            full_prediction = ode_trained(z_init, times, save_all = True, ode_solver = ode_solver)
+            plot_ODE_sol(observations = [observations], times = [times], pred_path = [full_prediction], figname = f'./pictures/{file_name}_{i}.png')
+            print(f'Epoch: [{i}/{n_epoch}], loss: {loss}')
     
 if __name__ == '__main__':
     # Generate timing data (for our task we generate trajectories over time)
@@ -158,4 +145,6 @@ if __name__ == '__main__':
     
     # Experiment 2 
     N_EPOCH = 1000
-    experiment2(n_epoch = N_EPOCH, file_name = "spiral_fit")
+    BATCH_SIZE = 32
+    plot_freq = 10
+    experiment2(n_epoch = N_EPOCH, batch_size = BATCH_SIZE, plot_freq = plot_freq, file_name = "example_spiral/spiral_fit")
